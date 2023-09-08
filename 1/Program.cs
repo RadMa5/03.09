15bbab033ba61e2aab4280fbad7b0c1ccbf24386
RadMa <maks25022@gmail.com>
2023-09-08 20:19:39 +0500

    Godawful fix of 1st program

--- 1/Program.cs
+++ 1/Program.cs
@@ -3,33 +3,36 @@
     for (int i = 0; i < array.GetLength(0); i++)
     {
         int Boundary = 0;
-        if (array.GetLength(1) >= Boundary)
+        if (Length1 > Boundary)
         {
             int MaxNum = array[i, Boundary];
-            for (int j = Boundary; j < array.GetLength(1); j++)
+            int MaxIndex = Boundary;
+            for (int j = Boundary; j < Length1; j++)
             {
-                int MaxIndex = 0;
-                if (array[i, j] > MaxNum) { array[i, j] = MaxNum; MaxIndex = j; }
+                if (array[i, j] > MaxNum) { MaxNum = array[i, j]; MaxIndex = j; }
             }
             array[i, MaxIndex] = array[i, Boundary];
             array[i, Boundary] = MaxNum;
+            MaxIndex = 0;
             Boundary++;
 
         }
+        int lastnum = array[i, Length1 - 1];
+        if (lastnum > array[i, Length1 - 2]) { array[i, Length1 - 1] = array[i, Length1 - 2]; array[i, Length1 - 2] = lastnum; } //Crime against humanity
     }
     return array;
 }
-void DisplayArray(Array)
+void DisplayArray(int[,] Array)
 {
-    for (int i = 0; i < array.GetLength(0); i++)
+    for (int i = 0; i < Array.GetLength(0); i++)
     {
-        for (int j = 0; j < array.GetLength(1); j++)
+        for (int j = 0; j < Array.GetLength(1); j++)
         {
-            Console.Write($"{array[i, j]} ");
+            Console.Write($"{Array[i, j]} ");
         }
         Console.WriteLine();
     }
 }
 int[,] Array = { { 1, 4, 7, 2 }, { 5, 9, 2, 3 }, { 8, 4, 2, 4 } };
 SortArray(Array);
-DisplayArray();
+DisplayArray(Array);
