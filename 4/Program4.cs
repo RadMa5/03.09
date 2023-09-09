03c18b3e5c7b908874c3850c5525297c628def68
RadMa <maks25022@gmail.com>
2023-09-09 09:36:16 +0500

    4th program prettified

--- 4/Program4.cs
+++ 4/Program4.cs
@@ -1,25 +1,25 @@
-﻿int[,] LazySpiral()
+﻿string[,] LazySpiral()
 {
-    int[,] matrix = new int[4, 4];
-    matrix[0, 0] = 01;
-    matrix[0, 1] = 02;
-    matrix[0, 2] = 03;
-    matrix[0, 3] = 04;
-    matrix[1, 3] = 05;
-    matrix[2, 3] = 06;
-    matrix[3, 3] = 07;
-    matrix[3, 2] = 08;
-    matrix[3, 1] = 09;
-    matrix[3, 0] = 10;
-    matrix[2, 0] = 11;
-    matrix[1, 0] = 12;
-    matrix[1, 1] = 13;
-    matrix[1, 2] = 14;
-    matrix[2, 2] = 15;
-    matrix[2, 1] = 16;
+    string[,] matrix = new string[4, 4];
+    matrix[0, 0] = "01";
+    matrix[0, 1] = "02";
+    matrix[0, 2] = "03";
+    matrix[0, 3] = "04";
+    matrix[1, 3] = "05";
+    matrix[2, 3] = "06";
+    matrix[3, 3] = "07";
+    matrix[3, 2] = "08";
+    matrix[3, 1] = "09";
+    matrix[3, 0] = "10";
+    matrix[2, 0] = "11";
+    matrix[1, 0] = "12";
+    matrix[1, 1] = "13";
+    matrix[1, 2] = "14";
+    matrix[2, 2] = "15";
+    matrix[2, 1] = "16";
     return matrix;
 }
-void DisplayArray(int[,] Array)
+void DisplayArray(string[,] Array)
 {
     for (int i = 0; i < Array.GetLength(0); i++)
     {
